33caf6377835ce54a86ac785ccffbb0cb9c8f9f6
Dwayne <d.h.cannon@icloud.com>
2022-10-03 15:43:46 -0600

    finish Objective2Lab3.java

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,15 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int birthYear= 1993;
+    String birthMonth = "october";
+    boolean monday = true;
+    boolean tuesday = true;
+    boolean wednesday = true;
+    boolean friday = true;
+    boolean saturday = false;
+    boolean sunday = false;
+    System.out.println(birthYear);
+    System.out.println(birthMonth);
+    System.out.println(monday);
+  }
+}
