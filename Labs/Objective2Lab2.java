f68fa78201c412acc816ddad1d5ae79dedee59dc
Dwayne <d.h.cannon@icloud.com>
2022-10-03 15:35:14 -0600

    finish Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+    public static void main(String[] args) {
+      String name = "Name: Dwayne Cannon Age: ";
+      int age = 28;
+      String state = " State: Colorado";
+      System.out.println(name + age + state);
+
+    }
+}
