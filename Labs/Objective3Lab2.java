eb73b238488e11c1fd9e15adaaf751f76471a970
Dwayne <d.h.cannon@icloud.com>
2022-10-03 16:08:19 -0600

    finish Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    int side1 = 10;
+    int side2 = 11;
+    int sum = side1*2+side2*2;
+    System.out.println("The hypotenuse of a triangle with sides of 10.0 and 11.0 is ");
+    System.out.println(sum);
+  }
+}
