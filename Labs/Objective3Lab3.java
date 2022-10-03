b273899243fa7b0c9192e7aa1ae52a1ad3e8cc80
Dwayne <d.h.cannon@icloud.com>
2022-10-03 17:06:49 -0600

    finish Objective3Lab3.java

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -3,12 +3,13 @@ public class Objective3Lab3 {
     int first = 1 + (int)(Math.random() * 6);
     int second = 1 + (int)(Math.random() * 6);
     int third = 1 + (int)(Math.random() * 6);
+    int sum = first + second + third;
     System.out.print(first);
     System.out.print(" + ");
     System.out.print(second);
     System.out.print(" + ");
     System.out.print(third);
     System.out.print(" = ");
-    System.out.print(first+second+third);
+    System.out.print(sum);
   }
 }
