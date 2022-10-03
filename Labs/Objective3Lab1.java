8cd90749fb98e7b587d282b3182a8cb1cce2c506
Dwayne <d.h.cannon@icloud.com>
2022-10-03 15:57:25 -0600

    finish Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,8 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    int num1 = 5;
+    int num2 = 13;
+    int num3 = 1;
+    System.out.println(num1+num2+num3/3);
+  }
+}
