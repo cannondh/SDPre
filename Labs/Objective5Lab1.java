3b165ef20173a96d71f49efe9db02fdd2ec3fc7c
Dwayne <d.h.cannon@icloud.com>
2022-10-05 14:29:45 -0600

    finish Objective5Lab1.java

---
+++ Labs/Objective5Lab1.java
@@ -0,0 +1,19 @@
+public class Objective5Lab1{
+
+  public static void main(String[] args) {
+
+    String string1 = "Banana";
+    String string2 = "Bacon";
+    String string3 = "Banana";
+
+    if(string1.equals(string2)) {
+      System.out.println(string1 + " is equivalent to " + string2);
+    }
+
+    else {
+      System.out.println(string1 + " is equivalent to " + string3);
+    }
+
+
+  }
+}
