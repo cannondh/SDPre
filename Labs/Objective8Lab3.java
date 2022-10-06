164097ec2240ee8b0ac75a5222579e33b52b386e
Dwayne <d.h.cannon@icloud.com>
2022-10-06 10:13:09 -0600

    finish Objective8Lab3.java

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,18 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+
+    int counter = 1;
+
+    for (int i=1; i<=20; i+=1) {
+      System.out.print(counter);
+      counter = counter + 1;
+      if(counter % 2 == 0) {
+        System.out.println(" is even ");
+      }
+
+      else {
+        System.out.println(" is odd ");
+      }
+    }
+  }
+}
