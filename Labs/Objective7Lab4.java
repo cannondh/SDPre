35d462e7ce6a8bb23df30fc4fd582dad2056912c
Dwayne <d.h.cannon@icloud.com>
2022-10-05 18:39:33 -0600

    finish Objective7Lab4.java

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    int counter = 0;
+
+    do{
+      counter = counter + 1;
+    }while(counter < 210);
+
+    if(counter == 210){
+      System.out.println(counter);
+    }
+  }
+}
